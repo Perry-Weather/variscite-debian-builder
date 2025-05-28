2df250ee83b23d8d358c726ac28e44994fbe9048
Zach Bullough <zbullough@perryweather.com>
2025-05-28 11:04:41 -0500

    fix watchdog parameter on boot args

--- variscite/mx6ul_mx7_install_debian.sh
+++ variscite/mx6ul_mx7_install_debian.sh
@@ -282,7 +282,7 @@ install_kernel_to_emmc()
 	cd ${IMGS_PATH}
 	cp -v ${KERNEL_DTBS}	${mountdir_prefix}${bootpart}
 	cp -v ${KERNEL_IMAGE}	${mountdir_prefix}${bootpart}
-	echo "bootargs=net.ifnames=0 nowayout=1" >> ${mountdir_prefix}${bootpart}/uEnv.txt
+	echo "bootargs=net.ifnames=0 imx2_wdt.nowayout=1" >> ${mountdir_prefix}${bootpart}/uEnv.txt
 	cd - >/dev/null
 	sync
 	umount ${node}${part}${bootpart}
